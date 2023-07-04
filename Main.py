0909d8774b3acf7dd2a5536e69d2e305fbd5209a
jaytipattni <jayti.pattni@gmail.com>
2023-07-04 21:17:35 +1200

    Created human vs human mode and human vs computer mode

--- Main.py
+++ Main.py
@@ -50,18 +50,130 @@ class StartPage(tk.Frame):
 class HumanVHuman(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        tk.Label(self, text="Human VS Human!").pack(pady=10)
-        tk.Button(self, text="Back",
-                  command=lambda: controller.show_frame(StartPage)).pack()
+        self.label = tk.Label(self, text="Human VS Human!")
+        self.label.grid(row=0, column=0, columnspan=3, pady=10)
 
-    
+        # Create the board
+        self.board_buttons = []
+        for i in range(3):
+            row = []
+            for j in range(3):
+                button = tk.Button(self, text=" ", width = 20, height = 3, command=lambda i=i, j=j: self.make_move(i, j))
+                button.grid(row=i, column=j)
+                row.append(button)
+            self.board_buttons.append(row)
+
+        # Create a reset button
+        tk.Button(self, text="Reset", command=self.reset).grid(row=3, column=0, columnspan=3)
+
+        # Create a back button
+        tk.Button(self, text="Back",
+                  command=lambda: controller.show_frame(StartPage)).grid(row=4, column=0, columnspan=3)
+
+        self.controller = controller
+        self.game = TicTacToe()
+        self.player1 = HumanPlayer('X')
+        self.player2 = HumanPlayer('O')
+        self.current_player = self.player1
+
+    # Make a move
+    def make_move(self, i, j):
+        index = i * 3 + j
+        if self.game.boardList[index] == '.':
+            self.game.makeTurn(index, self.current_player.letter)
+            self.board_buttons[i][j].config(text=self.current_player.letter)
+
+            if self.game.checkWin(self.current_player.letter):
+                tk.messagebox.showinfo("Game Over", f"{self.current_player.letter} wins!")
+                self.controller.show_frame(StartPage)
+                return
+            elif self.game.checkDraw():
+                tk.messagebox.showinfo("Game Over", "Draw!")
+                self.controller.show_frame(StartPage)
+                return
+
+            # switch player
+            self.current_player = self.player1 if self.current_player == self.player2 else self.player2
+
+    def reset(self):
+        pass
 
 class HumanVComputer(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        tk.Label(self, text="Human VS Computer!").grid(pady=10)
+        self.label = tk.Label(self, text="Human VS Human!")
+        self.label.grid(row=0, column=0, columnspan=3, pady=10)
+
+        # Create a toggle button which allows the user to choose who goes first
+        self.toggle = tk.IntVar()
+        self.toggle.set(1)
+        tk.Checkbutton(self, text="Human First", variable=self.toggle).grid(row=0, column=3, columnspan=3)
+
+        # Create the board
+        self.board_buttons = []
+        for i in range(3):
+            row = []
+            for j in range(3):
+                button = tk.Button(self, text=" ", width = 20, height = 3, command=lambda i=i, j=j: self.make_move(i, j))
+                button.grid(row=i, column=j)
+                row.append(button)
+            self.board_buttons.append(row)
+
+        # Create a reset button
+        tk.Button(self, text="Reset", command=self.reset).grid(row=3, column=0, columnspan=3)
+
+        # Create a back button
         tk.Button(self, text="Back",
-                  command=lambda: controller.show_frame(StartPage)).grid(pady=10)
+                  command=lambda: controller.show_frame(StartPage)).grid(row=4, column=0, columnspan=3)
+
+        self.controller = controller
+        self.game = TicTacToe()
+        self.player1 = HumanPlayer('X')
+        self.player2 = ComputerPlayer('O')
+        self.current_player = self.player1
+
+    # Make a move
+    def make_move(self, i, j):
+        index = i * 3 + j
+        if self.game.boardList[index] == '.':
+            self.game.makeTurn(index, self.current_player.letter)
+            self.board_buttons[i][j].config(text=self.current_player.letter)
+
+            if self.game.checkWin(self.current_player.letter):
+                tk.messagebox.showinfo("Game Over", f"{self.current_player.letter} wins!")
+                self.controller.show_frame(StartPage)
+                return
+            elif self.game.checkDraw():
+                tk.messagebox.showinfo("Game Over", "Draw!")
+                self.controller.show_frame(StartPage)
+                return
+
+            # switch player
+            self.current_player = self.player1 if self.current_player == self.player2 else self.player2
+
+            # Computer's turn
+            if isinstance(self.current_player, ComputerPlayer):
+                self.make_computer_move()
+
+    def make_computer_move(self):
+        index = self.current_player.getMove(self.game)
+        self.game.makeTurn(index, self.current_player.letter)
+        self.board_buttons[index // 3][index % 3].config(text=self.current_player.letter)
+
+        if self.game.checkWin(self.current_player.letter):
+            tk.messagebox.showinfo("Game Over", f"{self.current_player.letter} wins!")
+            self.controller.show_frame(StartPage)
+            return
+        elif self.game.checkDraw():
+            tk.messagebox.showinfo("Game Over", "Draw!")
+            self.controller.show_frame(StartPage)
+            return
+
+        # switch player
+        self.current_player = self.player1 if self.current_player == self.player2 else self.player2
+
+    def reset(self):
+        pass
 
 class ComputerVComputer(tk.Frame):
     def __init__(self, parent, controller):
@@ -92,8 +204,34 @@ class ComputerVComputer(tk.Frame):
         self.player1 = ComputerPlayer('X')
         self.player2 = ComputerPlayer('O')
 
+        self.current_player = self.player1
+
     def start_game(self):
-        pass
+        self.play_next_move()
+
+    def play_next_move(self):
+        # get move from the current player
+        index = self.current_player.getMove(self.game)
+        self.game.makeTurn(index, self.current_player.letter)
+
+        # update the board
+        i, j = divmod(index, 3)
+        self.board_buttons[i][j].config(text=self.current_player.letter)
+
+        if self.game.checkWin(self.current_player.letter):
+            tk.messagebox.showinfo("Game Over", f"{self.current_player.letter} wins!")
+            self.controller.show_frame(StartPage)
+            return
+        elif self.game.checkDraw():
+            tk.messagebox.showinfo("Game Over", "Draw!")
+            self.controller.show_frame(StartPage)
+            return
+
+        # switch player
+        self.current_player = self.player1 if self.current_player == self.player2 else self.player2
+
+        # schedule the next move
+        self.after(500, self.play_next_move)
 
 if __name__ == "__main__":
     app = Application()
